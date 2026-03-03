fcb27a6f3c055bf8ff8187f0d8404e64e7dafb6c
dev0xiinko <adrian.github.dev@gmail.com>
2026-03-03 21:13:49 +0800

    Add missing layout components and UI exports

--- platec_mobile/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Card, CardHeader, CardTitle, CardContent } from './Card';
-export { default as Badge } from './Badge';
